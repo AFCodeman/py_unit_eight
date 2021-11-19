ddc764f38a58654e8d7695703916691ba444eb21
anthonyfagiolo <fagioan@ssfs.org>
2021-11-19 10:59:00 -0500

    Finished creating the basic windows

--- tip_calculator.py
+++ tip_calculator.py
@@ -18,7 +18,7 @@ total = tk.StringVar()
 total.set("$0.0")
 
 word = tk.Label(root, text="Tip Calculator")
-word.grid(row=1,column=1)
+word.grid(row=1,column=1, columnspan=2)
 
 
 meal_cost_label = tk.Label(root, text="Meal Cost:")
@@ -38,15 +38,19 @@ tip_slider.grid(row=3,column=2)
 diners_label = tk.Label(root, text="Number of Diners")
 diners_label.grid(row=4,column=1)
 
-numb_diners1 = tk.Radiobutton(root, text="1", value=1, variable=people)
+
+button_frame = tk.Frame(root)
+button_frame.grid(row=5,column=1)
+
+numb_diners1 = tk.Radiobutton(button_frame, text="1", value=1, variable=people)
 numb_diners1.grid(row=5, column=1)
-numb_diners2 = tk.Radiobutton(root, text="2", value=2, variable=people)
+numb_diners2 = tk.Radiobutton(button_frame, text="2", value=2, variable=people)
 numb_diners2.grid(row=5, column=2)
-numb_diners3 = tk.Radiobutton(root, text="3", value=3, variable=people)
+numb_diners3 = tk.Radiobutton(button_frame, text="3", value=3, variable=people)
 numb_diners3.grid(row=5, column=3)
-numb_diners4 = tk.Radiobutton(root, text="4", value=4, variable=people)
+numb_diners4 = tk.Radiobutton(button_frame, text="4", value=4, variable=people)
 numb_diners4.grid(row=5, column=4)
-numb_diners5 = tk.Radiobutton(root, text="5", value=5, variable=people)
+numb_diners5 = tk.Radiobutton(button_frame, text="5", value=5, variable=people)
 numb_diners5.grid(row=5, column=5)
 
 total_label = tk.Label(root, text="Total Per Person")
@@ -56,6 +60,6 @@ total_display = tk.Label(root, textvariable=total)
 total_display.grid(row=6, column=2)
 
 calculate = tk.Button(root, text="Calculate Tip", command=calculate_tip)
-calculate.grid(row=7,column=1)
+calculate.grid(row=7,column=1, columnspan=2)
 
 root.mainloop()
